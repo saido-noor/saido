6811c52abfaae80e54f17bc137db7a05b5ac0cf0
saido-noor <saidoomarnoor@gmail.com>
2022-02-13 19:50:33 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main() {
+    var names : String = "Saido Omar"
+    println(names)
+    var age : String = "23"
+    println(age)
+    var number : String = "0769377160"
+    println(number)
+    var weightinKg : Int = 58
+    println(weightinKg)
+    var kenyan : Boolean = true
+    println(kenyan)
+
+
+}
